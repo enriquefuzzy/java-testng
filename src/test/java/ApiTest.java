8eb0d188a1564a315efbb378f11d6807bc29089f
Enrique Aguirre <enrique@yourfuzzy.com>
2023-08-10 16:20:53 -0700

    Setup RestAssured test and setup cucumber files for future implementation

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,54 @@
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.util.HashMap;
+
+public class ApiTest {
+    String baseUrl = "https://reqres.in/api";
+    @Test
+    public void getUsers() {
+        //https://reqres.in/api/users
+        given()
+        .when()
+            .get(baseUrl + "/users?page=2")
+        .then()
+            .statusCode(200)
+            .body("page", equalTo(2))
+            .log().all();
+    }
+
+    @Test
+    public void postUser() {
+        HashMap data = new HashMap();
+        data.put("name", "Enrique");
+        data.put("job", "QA Automation Engineer");
+        given()
+                .contentType("application/json")
+                .body(data)
+        .when()
+                .post(baseUrl + "/users")
+        .then()
+                .statusCode(201)
+                .body("name", equalTo("Enrique"))
+                .body("job", equalTo("QA Automation Engineer"))
+                .body("id", notNullValue())
+                .body("createdAt", notNullValue())
+                .log().all();
+    }
+
+    @Test
+    public void getUser() {
+        when()
+                .get(baseUrl + "/users/2")
+        .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2))
+                .body("data.first_name", equalTo("Janet"))
+                .body("data.last_name", equalTo("Weaver"))
+                .body("data.avatar", startsWith("https://"))
+                .log().all();
+    }
+}
---
+++ src/test/resources/features/api/Api.feature
@@ -0,0 +1,12 @@
+@api
+Feature: Automate sample API requests using https://reqres.in/
+
+  Background: User generates token for Authorisation
+
+  @users
+  Scenario: Verify authorized user can get a list of users
+    Given A list of books are available
+    When I add a book to my reading list
+    Then the book is added
+    When I remove a book from my reading list
+    Then the book is removed
