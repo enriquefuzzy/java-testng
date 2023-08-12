c3fd838d6ca12833254366f4a32c0329759151cd
Enrique Aguirre <enrique@yourfuzzy.com>
2023-08-11 18:26:59 -0700

    Add more api methods

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,23 +1,25 @@
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
-import java.net.URI;
 import java.util.HashMap;
 
 // Basic test for RestAssured, will be updated to be laid out better
 public class ApiTest {
+    //https://reqres.in/api/users
     String baseUrl = "https://reqres.in/api";
     @Test
     public void getUsers() {
-        //https://reqres.in/api/users
         given()
         .when()
             .get(baseUrl + "/users?page=2")
         .then()
             .statusCode(200)
             .body("page", equalTo(2))
+            .body("data[0].first_name", equalTo("Michael"))
             .log().all();
     }
 
@@ -50,6 +52,53 @@ public class ApiTest {
                 .body("data.first_name", equalTo("Janet"))
                 .body("data.last_name", equalTo("Weaver"))
                 .body("data.avatar", startsWith("https://"))
+                .log().all();
+    }
+
+    @Test
+    public void putUser() {
+        JSONObject data = new JSONObject();
+        data.put("name", "Oingo");
+        data.put("job", "Boingo");
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(data.toString()) //Convert JSONobject to String
+        .when()
+                .put(baseUrl + "/users/2")
+        .then()
+                .statusCode(200)
+                .body("name", equalTo("Oingo"))
+                .body("job", equalTo("Boingo"))
+                .body("updatedAt", notNullValue())
+                .header("Content-Type", "application/json; charset=utf-8")
+                .log().all();
+    }
+
+    @Test
+    public void patchUser() {
+        HashMap data = new HashMap();
+        data.put("name", "Oingo");
+        data.put("job", "Boingo");
+        given()
+                .contentType("application/json")
+                .body(data)
+        .when()
+                .patch(baseUrl + "/users/2")
+        .then()
+                .statusCode(200)
+                .body("name", equalTo("Oingo"))
+                .body("job", equalTo("Boingo"))
+                .body("updatedAt", notNullValue())
+                .log().all();
+    }
+
+    @Test
+    public void deleteUser() {
+        when()
+                .delete(baseUrl + "/users/3")
+        .then()
+                .statusCode(204)
                 .log().all();
     }
 }
